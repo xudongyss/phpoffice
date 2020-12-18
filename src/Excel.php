f91057ad6bd6e389c73bfc8c797421d244944822
xudongyss <xudong.yss@gmail.com>
2020-12-18 15:38:15 +0800

    Update Excel.php

--- src/Excel.php
+++ src/Excel.php
@@ -2,25 +2,47 @@
 namespace XuDongYss\PhpOffice;
 
 use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
 
 class Excel{
+	private Spreadsheet $spreadsheet;
+	
 	/**
-	 * 导入
-	 * @param file	$file	文件或者上传的文件流
+	 * 初始化
 	 */
+	public function __construct() {
+		
+	}
+	
 	/**
-	 * 
-	 * @param file	$file	文件或者上传的文件流
-	 * @return array|mixed  出错会抛出异常
+	 * 获取操作对象
 	 */
-	public static function import($file) {
+	public function getSpreadsheet() {
+		return $this->spreadsheet;
+	}
+	
+	/**
+	 * 返回 excel 表格数据
+	 * @param string		$file	文件或者上传的文件流
+	 * @return array|mixed  		数组|出错会抛出异常
+	 */
+	public function import($file) {
 		$_data = [];
 		
 		/* 加载文件 */
-		$spreadsheet = IOFactory::load($file);
-		
-		$_data = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+		$this->spreadsheet = IOFactory::load($file);
+		/* 设置当前单元格 */
+// 		$spreadsheet->setActiveSheetIndexByName('线下集市订单');
+		/* 将当前表格单元格数据转化成数组 */
+		$_data = $this->spreadsheet->getActiveSheet()->toArray(null, true, true, true);
 		
 		return $_data;
+	}
+	
+	/**
+	 * 导出到 Excel
+	 */
+	public static function export() {
+		
 	}
 }
